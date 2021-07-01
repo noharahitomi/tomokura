559fcb599c59642e1d06ae9d91636938da334b5f
noharahitomi <69335777+noharahitomi@users.noreply.github.com>
2021-07-01 23:02:07 +0900

    factorybot

--- spec/factories/spendings.rb
+++ spec/factories/spendings.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :spending do
-    
+    account_id             {2}
+    start_time             {'2021-07-01'}
+    shopping_category_id   {3}
+    amount                 {'1000'}
+    content                {'晩ご飯'}
+    association :user
   end
 end
--- spec/models/spending_spec.rb
+++ spec/models/spending_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Spending, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @spending = FactoryBot.build(:spending)
+  end
+
+  describe '支出の保存' do
+    context '支出が保存できる場合' do
+      it 'すべての情報が登録されていれば、保存ができる' do
+      end
+      it '備考欄が空でも保存ができる' do
+      end
+    end
+    
+    context '支出が保存できない場合' do
+      it 'accoutが1の場合、保存ができない' do
+
+      end
+      it 'start_timeが空では保存ができない' do
+      end
+      it 'shopping_category_idが1の場合、保存ができない' do
+      end
+      it 'amountが空では保存できない' do
+      end
+      it 'amountが1円未満の場合、保存ができない' do
+      end
+      it 'amountが1000000円以上の場合、保存ができない' do
+      end
 end
